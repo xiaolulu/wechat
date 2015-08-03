60eb3fc2d266fd0edf7dfdc74ad85e3f62a7f352
xiaolulu <jiangxiao2000@126.com>
2015-08-03 18:58:46 +0800

    sec

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require( 'express' ),
 	lib = require( './lib/tool'),
+	path = require( 'path' ),
+	bodyParser = require( 'body-parser' ),
     http    = require( 'http' );
 			
 var app = express();
@@ -8,21 +10,33 @@ server = http.Server( app );
 app.set( 'port', process.env.PORT || 4000 );
 app.set( 'views', __dirname + '/views' );
 app.set( 'view engine', 'ejs' );
+app.use( '/', express.static( path.join( __dirname, 'assets' ))); //静态文件路径
+app.use( bodyParser.urlencoded({ extended: false }));
 
-app.use( function( req, res ){
-        res.send(req.query.echostr);
+app.use( function( req, res, next ){
+        //res.send(req.query.echostr);
+	next();
 });
+
+app.get( '/index', function( req, res ){
+	res.render( 'index.ejs' )
+});
+
+app.post( '/createMenu', function( req, res ){
+	debugger
+	lib.createMenu( req, res );
+})
 
 function a(){
 	lib.reqConfig({
 		    path : '/cgi-bin/token?grant_type=client_credential&appid=wxfb96cc74703eb978&secret=d49d5f6febd267637d85c56af4370bce',
-	}, function(data){
+	},  function(data){
 		    console.log( data)
 	})
 
 }
-
-setTimeout( a, 2000 );
+a();
+//setTimeout( a, 2000 );
 
 
 server.listen( app.get( 'port'), function(){
